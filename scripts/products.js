9067efa7e676a5bf32dd7c9a4e9450304178efc6
Alpha Farm <116768332+alphafarm@users.noreply.github.com>
2023-12-02 14:55:14 -0300

    Update 2.0
    Dynamic products

---
+++ scripts/products.js
@@ -0,0 +1,172 @@
+var products = [
+    {
+        title: "PERFIS",
+        category: "Perfis",
+        items: [
+            {
+                name: "Perfil Restabelecido",
+                description: [
+                    "Acompanha DOC",
+                    "Manual de uso",
+                    "Aquecido",
+                    "Foto de perfil e capa",
+                    "Dados do criador",
+                    "Email de criação",
+                    "Cookies de acesso",
+                    "Códigos 2FA",
+                    "Restabelecido"
+                ],
+                originalPrice: 197,
+                price: 150,
+                paymentLink: [
+                    "https://go.perfectpay.com.br/PPU38CNGL8Q?hidecard=1",
+                    "https://checkout.alphafarm.pro/2wq7GrWzWDgBANP"
+                ],
+                isMostSell: false
+            },
+            {
+                name: "Perfil Confirmado",
+                description: [
+                    "Acompanha DOC",
+                    "Manual de uso",
+                    "Aquecido",
+                    "Foto de perfil e capa",
+                    "Dados do criador",
+                    "Email de criação",
+                    "Cookies de acesso",
+                    "Códigos 2FA",
+                    "Confirmado"
+                ],
+                originalPrice: 157,
+                price: 120,
+                paymentLink: [
+                    "https://go.perfectpay.com.br/PPU38CNGL8T?hidecard=1",
+                    "https://checkout.alphafarm.pro/RmA83EKOKzGPVpY"
+                ],
+                isMostSell: true
+            },
+            {
+                name: "Perfil Simples",
+                description: [
+                    "Acompanha Manual de uso",
+                    "Aquecido",
+                    "Foto de perfil e capa",
+                    "Dados do criador",
+                    "Email de criação",
+                    "Cookies de acesso",
+                    "Códigos 2FA",
+                ],
+                originalPrice: 120,
+                price: 80,
+                paymentLink: [
+                    "https://go.perfectpay.com.br/PPU38CNGL8U?hidecard=1",
+                    "https://checkout.alphafarm.pro/nQ7kZ7aVa9g0eJL"
+                ],
+                isMostSell: false
+            }
+        ]
+    },
+    {
+        title: "BUSINESS MANAGERS",
+        category: "BMs",
+        items: [
+            {
+                name: "BM'S ILIMITADAS!",
+                description: [
+                    "Destrava 5 CA's Ilimitadas",
+                    "Sobe conversão em 24 Hrs",
+                    "Sem Limite de Gastos",
+                    "Produto 100% Nacional"
+                ],
+                originalPrice: 800,
+                price: 597,
+                paymentLink: [
+                    "https://go.perfectpay.com.br/PPU38CNGL6Q?hidecard=1",
+                    "https://api.whatsapp.com/send?phone=5547991243979&text=Ol%C3%A1%20equipe%20Alpha%20Farm,%20quero%20o%20link%20de%20pagamento%20no%20cart%C3%A3o%20para%20a%20BM%20ILIMITADA!"
+                ],
+                isMostSell: false
+            },
+            {
+                name: "BM'S 1.3K DIÁRIOS!",
+                description: [
+                    "Destrava 5 CA's de 1.3K",
+                    "Sobe conversão em 24 Hrs",
+                    "Produto 100% Nacional"
+                ],
+                originalPrice: 450,
+                price: 357,
+                paymentLink: [
+                    "https://go.perfectpay.com.br/PPU38CNGL7B?hidecard=1",
+                    "https://api.whatsapp.com/send?phone=5547991243979&text=Ol%C3%A1%20equipe%20Alpha%20Farm,%20quero%20o%20link%20de%20pagamento%20no%20cart%C3%A3o%20para%20a%20BM%201.3k!"
+                ],
+                isMostSell: true
+            },
+            {
+                name: "BM 257 DIÁRIOS!",
+                description: [
+                    "Sobe conversão em 24 Hrs",
+                    "Produto 100% Nacional"
+                ],
+                originalPrice: 100,
+                price: 80,
+                paymentLink: [
+                    "https://go.perfectpay.com.br/PPU38CNGL7C?hidecard=1",
+                    "https://checkout.alphafarm.pro/eApQgzl8Y5ZEb76"    
+                ],
+                isMostSell: false
+            }
+        ]
+    },
+    {
+        title: "COMBOS",
+        category: "Combos",
+        items: [
+            {
+                name: "Combo Alpha",
+                description: [
+                    "Perfil Restabelecido",
+                    "BM Ilimitada",
+                    "Sobe conversão em 24 Hrs"
+                ],
+                originalPrice: 747,
+                price: 672,
+                paymentLink: [
+                    "https://go.perfectpay.com.br/PPU38CNGLCL?hidecard=1",
+                    "https://api.whatsapp.com/send?phone=5547991243979&text=Ol%C3%A1%20equipe%20Alpha%20Farm,%20quero%20o%20link%20de%20pagamento%20no%20cart%C3%A3o%20para%20o%20COMBO%20ALPHA!"    
+                ],
+                isMostSell: false
+            },
+            {
+                name: "Combo Matilha",
+                description: [
+                    "Perfil Confirmado",
+                    "BM 1.3k",
+                    "Sobe conversão em 24 Hrs"
+                ],
+                originalPrice: 477,
+                price: 453,
+                paymentLink: [
+                    "https://go.perfectpay.com.br/PPU38CNGLD6?hidecard=1",
+                    "https://api.whatsapp.com/send?phone=5547991243979&text=Ol%C3%A1%20equipe%20Alpha%20Farm,%20quero%20o%20link%20de%20pagamento%20no%20cart%C3%A3o%20para%20o%20COMBO%20MATILHA!"    
+                ],
+                isMostSell: true
+            },
+            {
+                name: "Combo Lobinho",
+                description: [
+                    "Perfil Simples",
+                    "BM 257",
+                    "Sobe conversão em 24 Hrs"
+                ],
+                originalPrice: 160,
+                price: 147,
+                paymentLink: [
+                    "https://go.perfectpay.com.br/PPU38CNGLDG?hidecard=1",
+                    "https://checkout.alphafarm.pro/nQ7kZ7aVjzg0eJL"    
+                ],
+                isMostSell: false
+            }]
+    }
+];
+
+console.log(products);
